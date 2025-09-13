8b6ecf6dab11716795051e26b468691202b85539
jakariyaa <jakariya3460@gmail.com>
2025-09-14 00:06:11 +0600

    initial

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,9 @@
-import express, { Application, Request, Response } from 'express';
-import dotenv from 'dotenv';
-import cookieParser from 'cookie-parser';
-import router from './routes';
-import errorHandler from './middlewares/error.middleware';
-import { successResponse, errorResponse } from './utils/responseHandler';
+import cookieParser from "cookie-parser";
+import dotenv from "dotenv";
+import express, { Application, Request, Response } from "express";
+import errorHandler from "./middlewares/error.middleware";
+import router from "./routes";
+import { errorResponse, successResponse } from "./utils/responseHandler";
 
 dotenv.config();
 
@@ -14,11 +14,11 @@ app.use(express.json());
 app.use(cookieParser());
 
 // Root route
-app.get('/', (req: Request, res: Response) => {
+app.get("/", (req: Request, res: Response) => {
   res.status(200).json(
     successResponse({
-      message: 'Digital Wallet API is running!',
-      version: '1.0.0'
+      message: "Digital Wallet API is running!",
+      version: "1.0.0",
     })
   );
 });
@@ -28,9 +28,7 @@ app.use(router);
 
 // 404 handler
 app.use((req: Request, res: Response) => {
-  res.status(404).json(
-    errorResponse('Route not found', 404)
-  );
+  res.status(404).json(errorResponse("Route not found", 404));
 });
 
 // Error handling middleware
