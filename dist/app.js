2370a171cd0436a0762c19bda54c73cc69448cee
jakariyaa <jakariya3460@gmail.com>
2025-09-25 22:47:05 +0600

    Updated: dist build

--- dist/app.js
+++ dist/app.js
@@ -12,8 +12,10 @@ const routes_1 = __importDefault(require("./routes"));
 const responseHandler_1 = require("./utils/responseHandler");
 dotenv_1.default.config();
 const app = (0, express_1.default)();
-// CORS
-app.use((0, cors_1.default)());
+// CORS configuration
+app.use((0, cors_1.default)({
+    origin: ["http://localhost:5173"],
+}));
 // Middleware
 app.use(express_1.default.json());
 app.use((0, cookie_parser_1.default)());
